20611eb9a73af95e8bfa0fb7a846ece6b9b21c7f
Tom Woodward <twwoodward@me.com>
2017-11-24 10:29:43 -0500

    abs path added

--- internet.php
+++ internet.php
@@ -26,6 +26,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
  
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 
 function net_neutral_enqueue_scripts() {
